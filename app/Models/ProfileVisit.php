b70b06c001e9d2f25f72285e3376efb330bbdda7
AdmiralShakal <kirsan1998@mail.ru>
2023-04-19 21:20:41 +0300

    relationship improves

--- app/Models/ProfileVisit.php
+++ app/Models/ProfileVisit.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class ProfileVisit extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function visitor()
+    {
+        return $this->belongsTo(User::class, 'visitor_id');
+    }
+
+    public function visited()
+    {
+        return $this->belongsTo(User::class, 'visited_id');
+    }
 }
--- app/Models/Publication.php
+++ app/Models/Publication.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Publication extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function visited()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
